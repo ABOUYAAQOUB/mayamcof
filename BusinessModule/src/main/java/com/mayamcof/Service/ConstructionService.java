09639962a16237d696ba1c3d41614959fe4b18fd
ABOUYAAQOUB Hamza <hamzaabouyaaqoub@gmail.com>
2023-07-12 20:24:36 +0100

    Creation de toutes les fonctionnalites de Construction (CRUD)

---
+++ BusinessModule/src/main/java/com/mayamcof/Service/ConstructionService.java
@@ -0,0 +1,62 @@
+package com.mayamcof.Service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mayamcof.IService.IConstruction;
+import com.mayamcof.Repository.ConstructionRepository;
+import com.mayamcof.model.Construction;
+
+@Service
+public class ConstructionService implements IConstruction{
+
+	private ConstructionRepository constructionRepository;
+	
+	public ConstructionService(ConstructionRepository constructionRepository) {
+		
+		this.constructionRepository = constructionRepository;
+	}
+
+	@Override
+	public List<Construction> getAll() {
+		
+		return this.constructionRepository.findAll();
+	}
+
+	@Override
+	public Construction getConstruction(Long id) {
+		
+		return this.constructionRepository.findById(id).get();
+	}
+
+	@Override
+	public Construction create(Construction construction) {
+		
+		return this.constructionRepository.save(construction);
+	}
+
+	@Override
+	public Construction update(Construction construction) {
+		
+		Construction constructionUpdated = this.constructionRepository.findById(construction.getId()).get();
+		
+		constructionUpdated.setConfirmer(construction.isConfirmer());
+		constructionUpdated.setConsommations(construction.getConsommations());
+		constructionUpdated.setFacture(construction.getFacture());
+		constructionUpdated.setPrix(construction.getPrix());
+		constructionUpdated.setQuantite(construction.getQuantite());
+		constructionUpdated.setTache(construction.getTache());
+		constructionUpdated.setTerrain(construction.getTerrain());
+		constructionUpdated.setUnite(construction.getUnite());
+		
+		return this.constructionRepository.save(constructionUpdated);
+	}
+
+	@Override
+	public void delete(Long id) {
+		
+		this.constructionRepository.delete(this.constructionRepository.findById(id).get());
+	}
+
+}
