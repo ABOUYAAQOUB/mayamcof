7da3a2e50c707bcc1f1a8f4c8d5d1540f9da5a05
yassine EL Bouazzaoui <67290757+Yassine-ELBouazzaoui@users.noreply.github.com>
2023-07-13 19:07:56 +0100

    CRUD tables travailler

---
+++ BusinessModule/src/main/java/com/mayamcof/IService/ITravailler.java
@@ -0,0 +1,14 @@
+package com.mayamcof.IService;
+
+import java.util.List;
+
+import com.mayamcof.model.Travailler;
+
+public interface ITravailler {
+	
+	public List<Travailler>getAll();
+	public Travailler getTravailler(Long id);
+	public Travailler create(Travailler Travailler);
+	public Travailler update(Travailler Travailler);
+	public void delete(Long id);
+}
